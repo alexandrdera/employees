f4b977b95cf96332cde35c2714e7888137403293
Alexandr Derachic <alexandr.dera@gmail.com>
2017-11-11 16:01:07 +0200

    Добавлено автозаполнение таблицы Positions

--- database/seeds/PositionsTableSeeder.php
+++ database/seeds/PositionsTableSeeder.php
@@ -12,5 +12,47 @@ class PositionsTableSeeder extends Seeder
     public function run()
     {
         //
+        //Заполнение таблицы Employees без использования фабрик
+        DB::table('positions')->insert(
+            array(                
+                    [
+                    	'position_name' => 'CEO',
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ],
+                    [
+                    	'position_name' => 'Director',
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ],
+                    [	
+                    	'position_name' => 'Vice president',
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ],
+                    [
+                    	'position_name' => 'Chief',
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ],
+                    [
+                    	'position_name' => 'Deputy chief',
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ],
+                    [
+                    	'position_name' => 'Head of the sector',
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ],
+                    [
+	                    'position_name' => 'Worker', 
+	                    'created_at' => date('Y-m-d H:i:s'),
+	                    'updated_at' => date('Y-m-d H:i:s')
+                    ]
+
+                )
+            );
+
     }
 }
